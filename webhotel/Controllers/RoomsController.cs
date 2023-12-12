94b1c103ff5c301c5814c9e51d6f2390a95f00c6
xuantruongg03 <108038337+xuantruongg03@users.noreply.github.com>
2023-12-12 14:08:02 +0700

    init

--- webhotel/Controllers/RoomsController.cs
+++ webhotel/Controllers/RoomsController.cs
@@ -1,16 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webhotel.Models;
 
 namespace webhotel.Controllers
 {
-    public class RoomsController : Controller
+	public class RoomViewModel
+	{
+		public IQueryable RoomDetail { get; set; }
+		public IEnumerable<Room> RoomOthers { get; set; }
+	}
+	public class RoomsController : Controller
     {
+
+		private readonly WebhotelContext _context;
+
+        public RoomsController(ILogger<RoomsController> logger)
+        {
+            _context = new WebhotelContext();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var rooms = _context.Rooms.Include(r => r.Roomimgs).Include(r => r.Type).ToList();
+            return View(rooms);
         }
+
+
+        [ActionName("Detail")]
         public IActionResult Detail(int? id)
         {
-            return View(id);
+            var roomsDetail = _context.Rooms.Include(r => r.Roomimgs).Include(r => r.Type).Where(r => r.Id == id).FirstOrDefault();
+			var roomOther = _context.Rooms.Include(r => r.Roomimgs).Include(r => r.Type).Take(2).ToList();
+			var rs = Tuple.Create(roomsDetail, roomOther);
+
+			return View(rs);
         }
     }
 }
